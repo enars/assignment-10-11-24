7dfe971535a70fb81d41b351b3c4b3474d358c3b
Ejnar Sörensen <ejnar.sorensen@nordnet.se>
2024-11-11 22:07:33 +0100

    setup order details widget

--- src/components/OrderDetails.tsx
+++ src/components/OrderDetails.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useOrderPanelContext } from "../context/OrderPanelContext";
 
 const DetailsContainer = styled.div`
   display: flex;
@@ -9,10 +10,39 @@ const DetailsContainer = styled.div`
   border-radius: 4px;
 `;
 
+const Button = styled.button`
+  padding: 6px;
+  margin: 6px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+`;
+
 const OrderDetails = () => {
+  const { selectedOrder } = useOrderPanelContext();
+
+  const handleDeleteOrder = () => {
+    //TODO
+  };
+
+  const handleEditOrder = () => {
+    //TODO
+  };
+
   return (
     <DetailsContainer>
       <h2>Order Details</h2>
+      {selectedOrder && (
+        <div>
+          <p>Instrument: {selectedOrder.instrumentId}</p>
+          <p>Amount: {selectedOrder.amount}</p>
+          <p>Price: {selectedOrder.price}</p>
+          <p>Action: {selectedOrder.action}</p>
+          <p>Created At: {selectedOrder.createdAt.toLocaleString()}</p>
+          <p>Updated At: {selectedOrder.updatedAt.toLocaleString()}</p>
+          <Button onClick={handleDeleteOrder}>Delete order</Button>
+          <Button onClick={handleEditOrder}>Edito order</Button>
+        </div>
+      )}
     </DetailsContainer>
   );
 };
